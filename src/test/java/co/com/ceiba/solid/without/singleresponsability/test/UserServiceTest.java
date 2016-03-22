5b1610ac34510344072f08a9d025a67eceee01cd
Andreina Del Valle Romero <andrrorm@suramericana.com.co>
2016-03-22 08:52:56 -0500

    Se añade esqueleto de la prueba para el ejemplo de singleresponsability

---
+++ src/test/java/co/com/ceiba/solid/without/singleresponsability/test/UserServiceTest.java
@@ -0,0 +1,10 @@
+package co.com.ceiba.solid.without.singleresponsability.test;
+
+import org.junit.Test;
+
+public class UserServiceTest {
+	@Test
+	public void shouldRegisterAnUser() {
+		//Deberá verificar que se pueda registrar un usuario satisfactoriamente
+	}
+}
